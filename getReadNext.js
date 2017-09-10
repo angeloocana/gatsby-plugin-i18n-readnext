35c8c3cfb7d3374dda0f994a2eafd9c76272f62e
Angelo Ocana <angeloocana@gmail.com>
2017-09-09 21:57:13 -0400

    Add getReadNext tests

--- getReadNext.js
+++ getReadNext.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getReadNext = exports.filterReadNext = exports.getReadNextRandom = undefined;
+exports.getReadNext = exports.getReadNextSelected = exports.getReadNextRandom = undefined;
 
 var _ramda = require('ramda');
 
@@ -13,12 +13,17 @@ var _ptzMath = require('ptz-math');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var getReadNextRandom = function getReadNextRandom(_ref) {
-  var nPosts = _ref.nPosts,
-      post = _ref.post,
-      readNext = _ref.readNext,
-      posts = _ref.posts;
+/*eslint max-params: ["error", 4]*/
 
+/**
+ * Concat random posts to readNext list
+ * @param {number} nPosts total number of posts
+ * @param {Post} post post node
+ * @param {[Post]} readNext read next post list
+ * @param {[Posts]} posts all posts
+ * @returns {[Posts]} random posts
+ */
+var getReadNextRandom = function getReadNextRandom(nPosts, post, readNext, posts) {
   var validPosts = posts.filter(function (p) {
     return p.fields.slug !== post.fields.slug && p.fields.langKey === post.fields.langKey;
   });
@@ -28,17 +33,32 @@ var getReadNextRandom = function getReadNextRandom(_ref) {
   return _ramda2.default.take(nPosts, _ramda2.default.concat(readNext, _ramda2.default.uniq(randomPosts)));
 };
 
-var filterReadNext = function filterReadNext(nPosts, readNext, posts) {
-  return !readNext || !posts ? [] : _ramda2.default.filter(function (p) {
-    return _ramda2.default.contains(p.fields.slug, readNext);
+/**
+ * get read next posts from selected posts in post.frontmatter.readNext
+ * @param {*} nPosts total number of posts
+ * @param {*} post post to get the selected read next slug list
+ * @param {*} posts all posts
+ * @returns {[Posts]} selected posts
+ */
+var getReadNextSelected = function getReadNextSelected(nPosts, post, posts) {
+  return !post.frontmatter.readNext || !posts ? [] : _ramda2.default.filter(function (p) {
+    return _ramda2.default.contains(p.fields.slug, post.frontmatter.readNext);
   }, posts);
 };
 
+/**
+ * get read next posts and concat random posts to complete if needed
+ * @param {*} nPosts total number of posts
+ * @param {*} post post to get the selected read next slug list
+ * @param {*} posts all posts
+ * @returns {[Posts]} selected posts + random posts
+ */
 var getReadNext = function getReadNext(nPosts, post, posts) {
-  var readNext = filterReadNext(nPosts, post.frontmatter.readNext, posts);
-  return readNext.length === nPosts ? readNext : getReadNextRandom({ nPosts: nPosts, post: post, readNext: readNext, posts: posts });
+  var readNext = getReadNextSelected(nPosts, post, posts);
+
+  return readNext.length === nPosts ? readNext : getReadNextRandom(nPosts, post, readNext, posts);
 };
 
 exports.getReadNextRandom = getReadNextRandom;
-exports.filterReadNext = filterReadNext;
+exports.getReadNextSelected = getReadNextSelected;
 exports.getReadNext = getReadNext;
--- src/getReadNext.js
+++ src/getReadNext.js
@@ -1,29 +1,54 @@
 import R from 'ramda';
 import {getRandomItem} from 'ptz-math';
 
-const getReadNextRandom = ({ nPosts, post, readNext, posts }) => {
+/*eslint max-params: ["error", 4]*/
+
+/**
+ * Concat random posts to readNext list
+ * @param {number} nPosts total number of posts
+ * @param {Post} post post node
+ * @param {[Post]} readNext read next post list
+ * @param {[Posts]} posts all posts
+ * @returns {[Posts]} random posts
+ */
+const getReadNextRandom = (nPosts, post, readNext, posts) => {
   const validPosts = posts.filter(p =>
     p.fields.slug !== post.fields.slug &&
       p.fields.langKey === post.fields.langKey);
   const randomPosts = R.range(0, nPosts + 2).map(_ => getRandomItem(validPosts));
   return R.take(nPosts, R.concat(readNext, R.uniq(randomPosts)));
 };
-  
-const filterReadNext = (nPosts, readNext, posts) => {
-  return !readNext || !posts
+
+/**
+ * get read next posts from selected posts in post.frontmatter.readNext
+ * @param {*} nPosts total number of posts
+ * @param {*} post post to get the selected read next slug list
+ * @param {*} posts all posts
+ * @returns {[Posts]} selected posts
+ */
+const getReadNextSelected = (nPosts, post, posts) => {
+  return !post.frontmatter.readNext || !posts
     ? []
-    : R.filter(p => R.contains(p.fields.slug, readNext), posts);
+    : R.filter(p => R.contains(p.fields.slug, post.frontmatter.readNext), posts);
 };
-  
+
+/**
+ * get read next posts and concat random posts to complete if needed
+ * @param {*} nPosts total number of posts
+ * @param {*} post post to get the selected read next slug list
+ * @param {*} posts all posts
+ * @returns {[Posts]} selected posts + random posts
+ */
 const getReadNext = (nPosts, post, posts) => {
-  const readNext = filterReadNext(nPosts, post.frontmatter.readNext, posts);
+  const readNext = getReadNextSelected(nPosts, post, posts);
+
   return readNext.length === nPosts
     ? readNext
-    : getReadNextRandom({ nPosts, post, readNext, posts });
+    : getReadNextRandom(nPosts, post, readNext, posts);
 };
 
 export {
   getReadNextRandom,
-  filterReadNext,
+  getReadNextSelected,
   getReadNext
 };
